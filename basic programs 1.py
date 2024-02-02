0df912a243e4e58eae6c36e35bfad71a8f0a6c66
raju <ramanjaneyaraju7@gmail.com>
2024-02-02 18:28:34 +0530

    added basic programs

---
+++ basic programs 1.py
@@ -0,0 +1,16 @@
+#calculate the amount of an item
+itemcode=int(input('enter item code:'))
+itemname=input('enter item name:')
+quantity=int(input('enter quantity:'))
+unitprice=int(input('enter unit price:'))
+
+amount=quantity*unitprice
+
+print('itemcode:',itemcode)
+print('itemname:',itemname)
+print('quantity:',quantity)
+print('unitprice:',unitprice)
+print('amount:',amount)
+
+
+
